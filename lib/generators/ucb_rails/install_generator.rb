4a16b85ba7a7b3359d4c3654984bf828dc051f74
Silas J. Matson <silas@infinite.red>
2015-10-20 16:03:36 -0700

    Add required method.

--- lib/generators/ucb_rails/install_generator.rb
+++ lib/generators/ucb_rails/install_generator.rb
@@ -27,6 +27,15 @@ module UcbRails
           readme "README"
         end
       end
+
+      def self.next_migration_number(path)
+        unless @prev_migration_nr
+          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        else
+          @prev_migration_nr += 1
+        end
+        @prev_migration_nr.to_s
+      end
       
       private
       
