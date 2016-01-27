e4bf60d9fbd6357026fb1d1b3c71ef76e86fde67
Peter Philips <peter@planet.io>
2016-01-26 23:25:43 -0500

    major refactor of javascript assets including move to bootstrap datetime picker

--- dummy/app/assets/javascripts/application.js
+++ dummy/app/assets/javascripts/application.js
@@ -10,10 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require dataTables/jquery.dataTables
-//= require dataTables/jquery.dataTables.api.fnSetFilteringDelay
-//= require dataTables/jquery.dataTables.bootstrap
+//= require ucb_rails
 //= require_tree .
