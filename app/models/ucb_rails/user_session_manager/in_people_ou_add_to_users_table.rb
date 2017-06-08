4fef671dc57043e571ef2a4b389e70c571c47874
Darin Wilson <darinwilson@gmail.com>
2017-06-08 11:36:20 -0700

    Make sure last_login_at is updated on login

--- app/models/ucb_rails/user_session_manager/in_people_ou_add_to_users_table.rb
+++ app/models/ucb_rails/user_session_manager/in_people_ou_add_to_users_table.rb
@@ -2,17 +2,19 @@ module UcbRails
   module UserSessionManager
 
     class InPeopleOuAddToUsersTable < ActiveInUserTable
-      
+
       def login(uid)
         self.uid = uid
-        
+
         if people_ou_entry.present?
-          UcbRails::UserLdapService.create_or_update_user_from_entry(people_ou_entry)
+          UcbRails::UserLdapService.create_or_update_user_from_entry(people_ou_entry).tap do |user|
+            user.touch(:last_login_at)
+          end
         else
           nil
         end
       end
-      
+
     end
 
   end
